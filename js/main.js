b2d81f70c8415485174eb30f777e5d12f49e2220
Arthur Guiot <arguiot@gmail.com>
2018-01-02 11:43:13 +0100

    Added HTML5Boilerplate for website + basic design

---
+++ js/main.js
@@ -0,0 +1,22 @@
+const $ = new DisplayJS(window);
+
+$.on(".hamburger", "click", e => {
+	if($.getProp(".menu").css.top != "0px") {
+		$.css(".menu", "top", "0")
+		$.css(".hamburger", "color", "black")
+		$.html(".hamburger", "<i class=\"fa fa-times\" aria-hidden=\"true\"></i>")
+	} else {
+		$.css(".menu", "top", "-100vh")
+		$.css(".hamburger", "color", "white")
+		$.html(".hamburger", "<i class=\"fa fa-bars\" aria-hidden=\"true\"></i>")
+	}
+})
+
+$.scroll(() => {
+    const distance = $.scrollTop();
+    if (distance > window.innerHeight - 20) {
+        $.css(".hamburger", "color", "black")
+    } else {
+        $.css(".hamburger", "color", "white")
+    }
+})
