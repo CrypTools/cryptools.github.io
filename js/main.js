6216aca103d97f9dff1e8fb5e9f7ca7144c7400c
Arthur Guiot <arguiot@gmail.com>
2018-01-02 13:02:40 +0100

    Added Glottologist

--- js/main.js
+++ js/main.js
@@ -1,15 +1,15 @@
 const $ = new DisplayJS(window);
 
 $.on(".hamburger", "click", e => {
-	if($.getProp(".menu").css.top != "0px") {
-		$.css(".menu", "top", "0")
-		$.css(".hamburger", "color", "black")
-		$.html(".hamburger", "<i class=\"fa fa-times\" aria-hidden=\"true\"></i>")
-	} else {
-		$.css(".menu", "top", "-100vh")
-		$.css(".hamburger", "color", "white")
-		$.html(".hamburger", "<i class=\"fa fa-bars\" aria-hidden=\"true\"></i>")
-	}
+    if ($.getProp(".menu").css.top != "0px") {
+        $.css(".menu", "top", "0")
+        $.css(".hamburger", "color", "black")
+        $.html(".hamburger", "<i class=\"fa fa-times\" aria-hidden=\"true\"></i>")
+    } else {
+        $.css(".menu", "top", "-100vh")
+        $.css(".hamburger", "color", "white")
+        $.html(".hamburger", "<i class=\"fa fa-bars\" aria-hidden=\"true\"></i>")
+    }
 })
 
 $.scroll(() => {
@@ -19,4 +19,10 @@ $.scroll(() => {
     } else {
         $.css(".hamburger", "color", "white")
     }
+})
+
+// Glottologist
+const glot = new Glottologist();
+glot.import("lang.json").then(() => {
+	glot.render()
 })
